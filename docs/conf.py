9836e9036f018df8bba9d76a3791ae6b075581a7
antony-with-no-h <94751539+antony-with-no-h@users.noreply.github.com>
2021-12-16 14:39:22 +0000

    add RTD conf files

---
+++ docs/conf.py
@@ -0,0 +1,23 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'ansible-db-oracle'
+copyright = '2021,  antony-with-no-h'
+author = ' antony-with-no-h'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = []
+
+templates_path = ['_templates']
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
